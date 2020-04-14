8c968c724ceab0ff5aaaddf9057032da740f5fc0
MAtterholt <matterholt84@gmail.com>
2020-04-14 00:04:13 -0400

    list style, and input style

--- md_editor/README.md
+++ md_editor/README.md
@@ -1,3 +1,12 @@
 # Mark down editor
 
 > note: this code was not submitted to the CareerHack and was being worked o several days after deadline!
+
+## TODO
+
+1. make the order list look more like a row id more than a list
+2. click on an saved mark down list and make it editable
+3. css even Odd row column scheme. (use list id if odd or even)
+4. mapping of the saved markdown file
+5. convert the input to actual markdown lol.Forgot
+6. list ID 13 has height issue and showing background
--- md_editor/pages/oneSheet.js
+++ md_editor/pages/oneSheet.js
@@ -17,7 +17,7 @@ function MkdownInput(props) {
     <div>
       <input
         className="input__container"
-        placeHolder="Add Content"
+        placeholder="Add Content"
         onKeyPress={enterKeyEvent}
         value={userInput}
         onChange={(e) => {
@@ -32,19 +32,67 @@ function MkdownInput(props) {
           background: gray;
           height: 30px;
           width: 100%;
-          margin-top: 20px;
+          margin-top: 25px;
+          padding: 10px 2px;
+          box-shadow: none;
+          border: none;
+          font-size: 15px;
+          font-weight: bold;
         }
-        .input__container.focus {
+        .input__container:focus {
           background: lightgray;
         }
       `}</style>
     </div>
   );
 }
+function MdLine(props) {
+  return (
+    <li className="mdLine__row" key={props.mdLineNum}>
+      <div className="mdLine__indicator">{props.mdLineNum}</div>
+      <span className="mdLine__string">{props.mdUserLine}</span>
+      <style jsx>{`
+        .mdLine__row {
+          display: flex;
+          margin: 2px 0;
+          overflow-wrap: break-word;
+          background-color: #b9cccc;
+        }
+        .mdLine__indicator {
+          padding: 5px;
+          color: #4a7677;
+          min-width: 16px;
+          margin-right: 5px;
+          background-color: #d2dfdf;
+          text-align: center;
+          height: 100%;
+          align-self: center;
+        }
+        .mdLine__string {
+          align-self: center;
+        }
+      `}</style>
+    </li>
+  );
+}
 function MkdownOutput(props) {
   const userMarkdowns = props.saveMdlines;
-  const mkDownList = userMarkdowns.map((x, y) => <li key={y}>{x}</li>);
-  return <ol>{mkDownList}</ol>;
+
+  return (
+    <ol>
+      {userMarkdowns.map((mdUserLine, mdLineNum) => (
+        <MdLine mdUserLine={mdUserLine} mdLineNum={mdLineNum} />
+      ))}
+      <style jsx>{`
+        ol {
+          margin: 0;
+          padding: 0;
+          list-style: none;
+          background: #f3f8f8;
+        }
+      `}</style>
+    </ol>
+  );
 }
 function OneSheet() {
   const [mdInputList, updateMdInputList] = useState([
@@ -62,6 +110,10 @@ function OneSheet() {
         <div className="sheet">
           <MkdownOutput saveMdlines={mdInputList} />
           <MkdownInput sendToMain={upateMdList} />
+          <div className="sheet__pre">
+            <button> Save</button>
+            <button> New </button>
+          </div>
         </div>
       </div>
       <style jsx>
@@ -74,9 +126,16 @@ function OneSheet() {
             justify-content: center;
           }
           .sheet {
-            border: solid black 1px;
             background: #f3f8f8;
-            min-width: 375px;
+            width: 375px;
+            height: max-content;
+            overflow: hidden;
+          }
+          .sheet__pre {
+            background: #f3f8f8;
+            width: 375px;
+            height: 150px;
+            background: linear-gradient(#f3f8f8, #a2c6c8);
           }
         `}
       </style>
